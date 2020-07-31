e88d38773019aa63e00cfffc8386b0da6060f266
Ruimin Zeng <ruimin6@vt.edu>
2020-07-30 21:03:55 -0400

    Push from local

--- locust_file.py
+++ locust_file.py
@@ -2,9 +2,9 @@ from locust import HttpUser, between
 import os
 
 def my_task(self):
-      self.client.post("/predict", {"age":"21", "sex":"male", "bmi":"18", "children":"0", "smoker":"no", "region":"southwest"})
+  self.client.post("/predict", {"age":"21", "sex":"male", "bmi":"18", "children":"0", "smoker":"no", "region":"southwest"})
 
 class MyLoadTest(HttpUser):
-  host = os.getenv("LOCUST_TARGET_HOST", "localhost:8080")
+  host = os.getenv("LOCUST_TARGET_HOST", "http://localhost:8080")
   tasks = [my_task]
   wait_time = between(5, 15)
