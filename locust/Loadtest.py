bf016016a40ac33c7a4ea590c434e0adcf5cb17d
YujieChen20 <67503195+YujieChen20@users.noreply.github.com>
2020-08-01 16:19:57 +0800

    Update Loadtest.py

--- locust/Loadtest.py
+++ locust/Loadtest.py
@@ -3,7 +3,7 @@ from locust import HttpUser, between, TaskSet, task
 class MetricsTaskSet(TaskSet):
     @task(1)
     def login(self):
-        self.client.post("/predict", {"age":"21", "sex":"male", "bmi":"18", "children":"0", "smoker":"no", "region":"southwest"})
+        self.client.post("/predict", data={"age":"21", "sex":"male", "bmi":"18", "children":"0", "smoker":"no", "region":"southwest"})
 
 class MyLoadTest(HttpUser):
   tasks = MetricsTaskSet
